84fe758a298ca63dbc6a3d8c801ac7dc2448ac89
bismansahni <bismansahni@outlook.com>
2024-02-12 16:01:58 -0700

    api call to a random website

---
+++ Api.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Api{
+    public static void main(String[] args) throws IOException, InterruptedException {
+        var url="https://catfact.ninja/fact";
+        //httprequest
+       var request= HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
+       var client=HttpClient.newBuilder().build();
+        var response=client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.statusCode());
+        System.out.println(response.body());
+
+
+    }
+}
